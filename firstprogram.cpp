28c5cbb223fdf11612acf987e4b6a8f63f8e4fe7
Arham-git <90620248+Arham-git@users.noreply.github.com>
2022-06-14 20:22:48 +0530

    Create firstprogram.cpp

---
+++ firstprogram.cpp
@@ -0,0 +1,18 @@
+//Word Counter in C++:
+//first program
+#include <iostream>
+using namespace std;
+int main()
+{
+    char str[1000];
+    int count = 0, i;
+    cout << "Enter a string : ";
+    gets(str);
+    for (i = 0; str[i] != '\0';i++)
+    {
+        if (str[i] == ' ')
+            count++;
+    }
+    cout << "Number of words in the string are: " << count + 1; //coz of no space after last word
+    return 0;
+}
